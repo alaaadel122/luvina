9c02407ef9613b306373f13a9de22e4d1af6c22a
alaaadel122 <alaaadel109@gmail.com>
2026-03-15 21:30:06 +0200

    feat: initial commit

--- src/components/auth/Layout.tsx
+++ src/components/auth/Layout.tsx
@@ -1,13 +1,15 @@
 import mainImg from "../../assets/images/Rectangle 1.png";
+import logoType from "../../assets/images/logotype.png";
 import Login from "./ui/login";
 
 export default function Layout() {
   return (
-    <div className='flex h-screen overflow-hidden'>
-        <div className="w-175 h-full">
-            <img src={mainImg} className='w-full h-full object-fit'/>
+    <div className='min-h-screen grid grid-cols-2'>
+        <div className="h-screen">
+            <img src={mainImg} className='w-full h-full object-center'/>
         </div>
-        <div className="w-[50%]  flex justify-center items-center">
+        <div className="flex items-center flex-col justify-center bg-gray-50">
+          <img src={logoType} className="absolute top-20"/>
           <Login/>
         </div>
     </div>
